a70a3014e0610f7e0cdb5dd38234daf27ba4c5b0
noiredex <thevertaurore@gmail.com>
2025-09-29 17:18:06 +0900

    수정) AdminService 기존 boolean approved -> Enum으로 변경

--- choplan/backend/src/main/java/choplan/db/application/properties/choplan/service/AdminService.java
+++ choplan/backend/src/main/java/choplan/db/application/properties/choplan/service/AdminService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import choplan.db.application.properties.choplan.dto.AuthResponse;
 import choplan.db.application.properties.choplan.dto.LoginRequest;
 import choplan.db.application.properties.choplan.dto.SignupRequestAdmin;
+import choplan.db.application.properties.choplan.entity.OwnerStatus;
 import choplan.db.application.properties.choplan.entity.UserRole;
 import choplan.db.application.properties.choplan.entity.Users;
 import choplan.db.application.properties.choplan.repository.UserRepository;
@@ -60,15 +61,15 @@ public class AdminService {
     }
 
     // Owner 승인 메서드 추가
-    public Users approveOwner(Long ownerId) {
+    public Users updateOwnerStatus(Long ownerId, OwnerStatus newStatus) {
         Users owner = userRepository.findById(ownerId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
-
+        .orElseThrow(() -> new IllegalArgumentException("해당 OWNER ID를 찾을 수 없습니다."));
+        
         if (!owner.getRole().equals(UserRole.OWNER)) {
-            throw new IllegalArgumentException("해당 사용자는 OWNER가 아닙니다.");
+            throw new IllegalArgumentException("해당 계정은 OWNER 계정이 아닙니다.");
         }
 
-        owner.setApproved(true);
+        owner.setOwnerStatus(newStatus);
         return userRepository.save(owner);
     }
 }

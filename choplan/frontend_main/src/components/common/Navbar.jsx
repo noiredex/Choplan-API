1ab9bb1fe9e3f31e1fc3906d336ac0f5eb423baa
noiredex <thevertaurore@gmail.com>
2025-10-21 15:41:20 +0900

    수정) Navbar는 Sidebar 에서 setSeletedCategory를 내려받음

--- choplan/frontend_main/src/components/common/Navbar.jsx
+++ choplan/frontend_main/src/components/common/Navbar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import "../../styles/Navbar.css";
 
-export default function Navbar() {
+export default function Navbar({ setSelectedCategory }) {
   return (
     <nav className="navbar">
-      <Sidebar />
+      <Sidebar setSelectedCategory={setSelectedCategory} />
       <div className="navbar-title">Choplan</div>
       <input type="text" placeholder="식당, 지역, 키워드 검색" className="search-input" />
       <div className="profile-icon">👤</div>
